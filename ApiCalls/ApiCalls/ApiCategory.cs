0545e51b6fa35403d501c80ca9c222b1d0c754be
IoanaCapitanu1 <ioana_capitanu@yahoo.com>
2024-05-10 17:32:17 -0700

    first commit

---
+++ ApiCalls/ApiCalls/ApiCategory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCalls
+{
+    public class ApiCategory
+    {
+        public String GetCategories()
+        {
+            var httpClient = new HttpClient();
+            var response = httpClient.GetAsync("https://opentdb.com/api_category.php").Result;
+
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Failed to retrieve trivia categories. Status code: " + response.StatusCode);
+            }
+            return content;
+
+        }
+    }
+}
---
+++ ApiCalls/ApiCalls/QuestionApi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ApiCalls
+{
+    public class QuestionApi
+    {
+        public String GetQuestions( int _categorie, String dificultate, Boolean isAny = false )
+        {
+            if (isAny == false)
+            {
+                var httpClient = new HttpClient();
+                var response = httpClient.GetAsync("https://opentdb.com/api.php?amount=10&category=" + _categorie + "&difficulty=" + dificultate + "&type=multiple").Result;
+                byte[] responseBytes = response.Content.ReadAsByteArrayAsync().Result;
+                var content = Encoding.UTF8.GetString(responseBytes);
+                return content;
+            }
+            else
+            {
+                var httpClient = new HttpClient();
+                var response = httpClient.GetAsync("https://opentdb.com/api.php?amount=20&category=" + _categorie + "&type=multiple").Result;
+                byte[] responseBytes = response.Content.ReadAsByteArrayAsync().Result;
+                var content = Encoding.UTF8.GetString(responseBytes);
+                return content;
+            }
+        }
+    }
+}
